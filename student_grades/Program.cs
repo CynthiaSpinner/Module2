4656aa1e40aa8275cea1b3c41b1822db035d7ec9
Cynthia <CynthiaSpinner@gmail.com>
2024-04-05 16:14:53 -0500

    properties, list and 2 methods

---
+++ student_grades/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace student_grades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ student_grades/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace student_grades
+{
+    public class Student
+    {
+
+        public string? Name { get; set; } //change null with value input
+        public int Id { get; set; }
+        public List<double> Grades { get; set; } = []; //short for instantiating??? 
+
+
+
+        public void AddGrade(double grade)
+        {
+
+           Grades.Add(grade);
+        }
+
+        public void AddGrade(params double[] grades)
+        {
+            Grades.AddRange(grades);
+        }
+    }
+
+
+}
